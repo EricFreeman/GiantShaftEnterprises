c02ff0a12ad3688df148885a10752a4f2f2cd63a
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-09 13:49:26 -0600

    moved some files around and started styling the page

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -4,15 +4,15 @@ function GameController($scope, $timeout) {
 
 	// All base items (not upgrades) you can purchase are defined here
 	$scope.items = [
-		{ "name" : "minimum wage worker", "count" : 1, "mps" : 1 },
-		{ "name" : "cubicals", "count" : 5, "mps" : 5 },
-		{ "name" : "salary employee", "count" : 2, "mps" : 50}
+		{ "name" : "minimum wage worker", "count" : 1, "mps" : 1 , "price" : 15},
+		{ "name" : "cubicals", "count" : 5, "mps" : 5 , "price" : 100},
+		{ "name" : "salary employee", "count" : 2, "mps" : 50, "price" : 1500}
 	];
 
-	// Your cumulitive mps (money per second) is the combination of the 
+	// Your cumulative mps (money per second) is the combination of the 
 	// count of each item multiplied by the item's individual mps
 	$scope.mps = function() {
-		rtn = $scope.items.reduce(function (prev, cur) {
+		return $scope.items.reduce(function (prev, cur) {
 			return prev += cur.mps * cur.count;
 		}, 0);
 	};
