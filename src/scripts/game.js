3240a34585ddeb76c0340c99601b7cb9fc4cb561
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-12 15:21:15 -0600

    added routing and a shared service for the different controllers to use that contains all the player information

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,28 +1,73 @@
-function GameController($scope, $timeout) {
-	$scope.money = 0;
-	$scope.fps = 10;
+var idleGame = angular.module('idleGame', ['ngRoute']);
 
+idleGame.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/store', {
+				templateUrl: 'store.html',
+				controller: 'StoreController'
+			}).
+			when('/stats', {
+				templateUrl: 'stats.html',
+				controller: 'StatsController'
+			}).
+			when('/menu', {
+				templateUrl: 'menu.html',
+				controller: 'MenuController'
+			}).
+			otherwise({
+				redirectTo: '/store'
+			});
+	}
+]);
+
+idleGame.service('gameService', function() {
+	this.money = 0;
+	this.fps = 10;
+	
 	// All base items (not upgrades) you can purchase are defined here
-	$scope.items = [
+	this.items = [
 		{ "name" : "minimum wage worker", "count" : 1, "mps" : 1 , "price" : 15},
 		{ "name" : "cubicals", "count" : 5, "mps" : 5 , "price" : 100},
 		{ "name" : "salary employee", "count" : 2, "mps" : 50, "price" : 1500}
 	];
-
+	
 	// Your cumulative mps (money per second) is the combination of the 
 	// count of each item multiplied by the item's individual mps
-	$scope.mps = function() {
-		return $scope.items.reduce(function (prev, cur) {
+	this.mps = function() {
+		return this.items.reduce(function (prev, cur) {
 			return prev += cur.mps * cur.count;
 		}, 0);
 	};
+});
+
+function GameLoopController($scope, $timeout, gameService) {	
+	// Getters to grab values through gameService
+	$scope.getMoney = function() {
+		return gameService.money;
+	}
+	$scope.getMps = function() {
+		return gameService.mps();
+	}
 	
 	// Game Loop
 	$scope.update = function() {
-		$scope.money += ($scope.mps() / $scope.fps);
-		$timeout($scope.update, 1000 / $scope.fps);
+		gameService.money += (gameService.mps() / gameService.fps);
+		$timeout($scope.update, 1000 / gameService.fps);
 	};
 	
 	// Start game loop
 	$scope.update();
 }
+
+function StoreController($scope, gameService) {
+	
+};
+
+function StatasController($scope, gameService) {
+	
+};
+
+function MenuController($scope, gameService) {
+	
+};
