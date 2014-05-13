bb6b6c9fbc64f63807f7a558b5d49f84f182f702
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-13 14:46:07 -0600

    Added saving and loading to the game

--- src/scripts/gameLoop.js
+++ src/scripts/gameLoop.js
@@ -9,16 +9,43 @@ function GameLoopController($scope, $timeout, gameService) {
 	$scope.getMps = function() {
 		return gameService.mps();
 	};
+
+	$scope.loadGame = function() {
+		// Go through each property of gameService, and if it exists, load it
+		for(var prop in gameService) {
+			var propName = "CompanyGame." + prop;
+			if(localStorage[propName] != undefined)
+				gameService[prop] = JSON.parse(localStorage.getItem(propName));
+		}
+	};
+
+	$scope.saveGame = function() {
+		// Save everything in the game service that isn't a function as JSON to local storage
+		for(var prop in gameService) {
+			if(typeof(gameService[prop]) != "function")
+				localStorage.setItem("CompanyGame." + prop, JSON.stringify(gameService[prop]));
+		}
+
+		$timeout($scope.saveGame, 30000); // Recall this method to autosave every 30 seconds
+	}
 	
 	// Game Loop
 	$scope.update = function() {
+		// Make sure the FPS is valid, otherwise default it
 		if(!isNumeric(gameService.fps)) gameService.fps = 10;
+
 		gameService.money += (gameService.mps() / gameService.fps);
 		$timeout($scope.update, 1000 / gameService.fps);
 	};
 	
+	// Load the game if it was previously saved
+	$scope.loadGame();
+
 	// Start game loop
 	$scope.update();
+
+	// Start save loop of every 30 seconds
+	$timeout($scope.saveGame, 30000);
 }
 
 function isNumeric(n) {
