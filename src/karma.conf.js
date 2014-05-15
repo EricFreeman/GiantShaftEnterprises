6963fbbe417a3575809554ce55c90ac83a8c0c8e
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-15 17:39:02 -0600

    start of unit tests...they don't work

---
+++ src/karma.conf.js
@@ -0,0 +1,7 @@
+files: [
+	'scripts/angular.min.js',
+	'scripts/angular-mocks.js',
+	'scripts/idleGame.js',
+	'scripts/store.js',
+	'spec/storeSpec.js'
+];
---
+++ src/spec/storeSpec.js
@@ -0,0 +1,8 @@
+describe("StoreController", function() {
+	var module;
+	beforeEach(module = angular.module('idleGame'));
+
+	it("should actually exist", function() {
+		expect(idleGame.StoreController).toBeDefined();
+	});
+});
