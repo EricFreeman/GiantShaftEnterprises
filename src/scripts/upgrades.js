d039e606afc89690f85c7f837b13db50c9e55d4d
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-16 08:31:50 -0600

    Only show upgrades for items you've already bought

--- src/scripts/upgrades.js
+++ src/scripts/upgrades.js
@@ -16,7 +16,14 @@ function UpgradesController($scope, gameService, playerService) {
 		}
 	};
 
+	// Get the name of the item the upgrade applies to.
+	// id is the itemId
 	$scope.getItemName = function(id) {
 		return gameService.getItem(id).name;
+	}
+
+	// Only show upgrades for items you've actually bought
+	$scope.canShow = function(id) {
+		return playerService.getItem(id).count > 0;
 	}
 };
