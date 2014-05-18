177cad3617ec0c73a2f52a7a41e791acce42e2b4
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-17 20:44:42 -0600

    tests don't work because I don't know how I should be mocking this service exactly, and unit tests are useless in the first place (in my not so humble opinion) so I'm done wasting my time for now.

--- src/spec/storeSpec.js
+++ src/spec/storeSpec.js
@@ -1,5 +1,41 @@
 describe("StoreController", function() {
+	var $scope;
+    var $provide;
+
+    beforeEach(angular.mock.module('idleGame'));
+
+    beforeEach(angular.mock.module(function (_$provide_) {
+        $provide = _$provide_;
+    }));
+
+    beforeEach(angular.mock.inject(function($rootScope, $controller, $q){
+        var mockPlayerService = {
+            money: 0,
+            items: [],
+            getItem: function(id) { return {count: sessionStorage["money"]}; }
+        };
+
+        $provide.value('playerService', mockPlayerService);
+        $scope = $rootScope.$new();
+        $controller('StoreController', { $scope: $scope });
+        $rootScope.$apply();
+    }));
+
 	it("should actually exist", function() {
-		expect(StoreController).not.toBeNull();
+		expect($scope).not.toBeNull();
+	});
+
+	it("should not be able to buy stuff when broke", function() {
+		expect($scope.boughtItems.length).toBe(0);
+		sessionStorage["money"] = 0;
+	 	$scope.buy({shiftKey: false}, 0);
+	 	expect($scope.boughtItems.length).toBe(0);
+	});
+
+	it("should be able to buy stuff when you're rich", function() {
+		expect($scope.boughtItems.length).toBe(0);
+		sessionStorage["money"] = 500;
+	 	$scope.buy({shiftKey: false}, 0);
+	 	expect($scope.boughtItems.length).toBe(1);
 	});
 });
