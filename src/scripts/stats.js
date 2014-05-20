4cafeb2485c45a7196c79c20041616a74edabed9
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-20 08:48:19 -0600

    A few more stats for total buildings and upgrads bought and possible upgrades

--- src/scripts/stats.js
+++ src/scripts/stats.js
@@ -1,3 +1,7 @@
 function StatsController($scope, gameService, playerService, mpsService) {
 	$scope.playerService = playerService;
+
+	$scope.totalBuildings = playerService.items.reduce(function(prev, curr) {return prev += curr.count}, 0);
+	$scope.totalUpgrades = playerService.upgrades.length;
+	$scope.possibleUpgrades = gameService.upgrades.length;
 };
