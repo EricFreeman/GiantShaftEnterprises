d8a2010c216a3b12e9b01ea2f769dd27b54296e0
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-20 09:33:22 -0600

    Added tests for achievements controller

---
+++ src/spec/achievementsSpec.js
@@ -0,0 +1,38 @@
+describe("AchievementsController", function() {
+	var $scope;
+    var $provide;
+
+    beforeEach(angular.mock.module('idleGame'));
+
+    beforeEach(angular.mock.module(function (_$provide_) {
+        $provide = _$provide_;
+    }));
+
+    beforeEach(angular.mock.inject(function($rootScope, $controller, $q){
+        $scope = $rootScope.$new();
+        $controller('AchievementsController', { $scope: $scope });
+        $rootScope.$apply();
+    }));
+
+    it("should actually exist", function() {
+		expect($scope).not.toBeNull();
+	});
+
+	it("should be able to award achievements", function() {
+		inject(function(playerService) {
+			playerService.achievements = [];
+			playerService.awardAchievement(0);
+		 	expect($scope.hasEarned(0)).toBe(true);
+		});
+	});
+
+	it("should not award achievements more than once", function() {
+		inject(function(playerService) {
+			playerService.achievements = [];
+			playerService.awardAchievement(0);
+			playerService.awardAchievement(0);
+		 	expect($scope.hasEarned(0)).toBe(true);
+		 	expect(playerService.achievements.length).toBe(1);
+		});
+	});
+});
