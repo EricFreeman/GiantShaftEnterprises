3bde8ec4085d9ec9861a18e1326c54288eac775d
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-22 09:47:37 -0600

    Combined sotre, upgrades, and business tab together and menu and about tab together.

--- src/scripts/about.js
+++
@@ -1,6 +0,0 @@
-function AboutController($scope) {
-	$scope.updates = [
-		{ title: '5/21/2014', description: 'Added this page!  And toast messages.' },
-		{ title: '5/9/2014 - 5/21/2014', description: 'I am not writing a description for the first almost two weeks of production, but rest assured I did a lot of work during this time.'}
-	];
-};
--- src/scripts/business.js
+++ src/scripts/business.js
@@ -1,4 +1,5 @@
 function BusinessController($scope, gameService, playerService, moneyService) {
+	// BUSINESS
 	$scope.possibleOpportunities = [
 		"Meet with clients.",
 		"Meet with investors.",
@@ -30,6 +31,93 @@ function BusinessController($scope, gameService, playerService, moneyService) {
 		while($scope.opportunity == curr)
 			$scope.opportunity = $scope.possibleOpportunities.randomElement();
 	};
+
+	// STORE
+	$scope.items = gameService.items;
+	$scope.boughtItems = playerService.items;
+	
+	$scope.currentPrice = function(id) {
+		var priceIncrease = .2; // 20% per item already bought
+		return gameService.getItem(id).price + (playerService.getItem(id).count * gameService.getItem(id).price * priceIncrease);
+	}
+	
+	$scope.getMps = function(id) {
+		return gameService.getItem(id).mps + $scope.getUpgradesMps(id);
+	}
+
+	// Get the mps that should be added in from the upgrades for the specified item
+	$scope.getUpgradesMps = function(id) {
+		return playerService.upgrades.reduce(function(prev, cur) {
+			var upgrade = gameService.getUpgrade(cur.id);
+			return prev += upgrade.itemId == id ? upgrade.mps : 0;
+		}, 0);
+	};
+
+	$scope.getCount = function(id) {
+		return playerService.getItem(id).count;
+	}
+
+	$scope.cantBuyStoreItem = function(id) {
+		return $scope.currentPrice(id) > playerService.money;
+	};
+
+	$scope.buyStoreItem = function($event, id) {
+		var count = 1;
+		if($event.shiftKey) count = 10;
+		else if($event.altKey) count = 100;
+
+		for(var i = 0; i < count; i++) {
+			if(!$scope.cantBuyStoreItem(id)) {
+				playerService.money -= $scope.currentPrice(id);
+				playerService.buyItem(id);
+			}
+		}
+	};
+
+	// UPGRADES
+	$scope.upgrades = gameService.upgrades.sort(function(a, b) {
+		if(a.price > b.price) return 1;
+		if(a.price < b.price) return -1;
+		return 0;
+	});
+	$scope.boughtUpgrades = playerService.upgrades;
+
+	$scope.alreadyBought = function(id) {
+		return playerService.getUpgrade(id).id != -1;
+	}
+
+	$scope.cantBuyUpgrade = function(id) {
+		var notEnoughMoney = gameService.getUpgrade(id).price > playerService.money;
+		return notEnoughMoney || $scope.alreadyBought(id);
+	};
+	
+	$scope.buyUpgrade = function(id) {
+		if(!$scope.cantBuyUpgrade(id)) {
+			var upgrade = gameService.getUpgrade(id);
+			playerService.money -= upgrade.price;
+			playerService.buyUpgrade(upgrade);
+		}
+	};
+
+	// Get the name of the item the upgrade applies to.
+	// id is the itemId
+	$scope.getItemName = function(id) {
+		var name = gameService.getItem(id).name
+		return name ? name : id;
+	}
+
+	// Only show upgrades for items you've actually bought
+	$scope.canShowUpgrade = function(upgrade) {
+		// Make sure you own all pre requisit upgrades for this specific upgrade
+		var haveAllPrereq = true;
+		if(gameService.getUpgrade(upgrade.id).showAfter) {
+			haveAllPrereq = gameService.getUpgrade(upgrade.id).showAfter.reduce(function(prev, cur) {
+				return prev && $scope.alreadyBought(cur);
+			}, true);
+		}
+
+		return haveAllPrereq && (playerService.getItem(upgrade.itemId).count > 0 || !!upgrade.isBusiness);
+	}
 };
 
 Array.prototype.randomElement = function () {
