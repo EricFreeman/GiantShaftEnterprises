6c33355f477ef82f6fa9f9b8369e4869d8d2f5ff
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-06 17:17:25 -0600

    Moved the store over a bit when doing the tutorial so the arrow will fit better on smaller monitors

--- src/scripts/overlay.js
+++ src/scripts/overlay.js
@@ -7,6 +7,12 @@ function OverlayController($scope, $timeout, playerService) {
 		return playerService.showTutorial;
 	}
 
+	$scope.goBackToBusiness = function() {
+		pos = $('.links').children('a').first().position();
+		$('.tutorial').css({left: pos.left - 360, top: pos.top - 16});
+		$scope.tutorialText = "Go back to the Business tab!";
+	}
+
 	$scope.tutorialInit = function() {
 		if(playerService.money >= 15) playerService.tutorialStep = 1;
 		if(playerService.tutorialStep == 1 && playerService.items.length > 0) playerService.tutorialStep = 2;
@@ -24,45 +30,39 @@ function OverlayController($scope, $timeout, playerService) {
 			$('.mainContent').show();
 			$('.upgrade').hide();
 			$('.achievementsContainer').hide();
+			$('.store').css('margin-left', '200px');
 
 			var pos = $('.store').children().first().position();
 			if(pos != undefined) {
 				$('.tutorial').css({left: pos.left - 330, top: pos.top});
 				$scope.tutorialText = "Purchase items to earn money for you automatically.";
 			}
-			else {
-				pos = $('.links').children('a').first().position();
-				$('.tutorial').css({left: pos.left - 360, top: pos.top - 16});
-				$scope.tutorialText = "Go back to the Business tab!";
-			}
+			else
+				$scope.goBackToBusiness();
 		}
 		else if(playerService.tutorialStep == 2) {
 			$('.upgrade').show();
+			$('.store').css('margin-left', '75px');
 
 			var pos = $('.upgrade').position();
 			if(pos != undefined) {
 				$('.tutorial').css({left: pos.left - 330, top: pos.top});
-				$scope.tutorialText = "Purchase upgrades to increase the power of your items, business opportunities, and passive bonuses!";
+				$scope.tutorialText = "Purchase upgrades to increase the power of your items, BUSINESS OPPORTUNITIES, and passive bonuses!";
 			}
-			else {
-				pos = $('.links').children('a').first().position();
-				$('.tutorial').css({left: pos.left - 360, top: pos.top - 16});
-				$scope.tutorialText = "Go back to the Business tab!";
-			}
+			else
+				$scope.goBackToBusiness();
 		}
 		else if(playerService.tutorialStep == 3) {
 			$('.achievementsContainer').show();
+			$('.store').css('margin-left', 'auto');
 
 			var pos = $('.achievementsContainer').position();
 			if(pos != undefined) {
 				$('.tutorial').css({left: pos.left - 330, top: pos.top});
-				$scope.tutorialText = "These are the achievements you've earned thus far.  You can get them in a variety of ways.  The next easy one for you to earn is by owning 10 Minimum Wage Workers, or doing BUSINESS OPPORTUNITIES 100 times.  Earn one more achievement to complete this tutorial.";
+				$scope.tutorialText = "These are the achievements you've earned thus far.  You can get them in a variety of ways.  The next easy one for you to earn is by owning 10 Minimum Wage Workers or doing BUSINESS OPPORTUNITIES 100 times.  Earn one more achievement to complete this tutorial.";
 			}
-			else {
-				pos = $('.links').children('a').first().position();
-				$('.tutorial').css({left: pos.left - 360, top: pos.top - 16});
-				$scope.tutorialText = "Go back to the Business tab!";
-			}
+			else
+				$scope.goBackToBusiness();
 		}
 		else {
 			$('.tutorial').remove();
