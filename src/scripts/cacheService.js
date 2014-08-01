1b88947e442906b4976b57509f798eb6050a348c
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-01 08:52:25 -0600

    Moved around some more files and added a boatload of new business opportunity flavor text.

--- src/scripts/cacheService.js
+++ src/scripts/cacheService.js
@@ -1,5 +1,61 @@
 var idleGame = angular.module('idleGame');
 
+idleGame.service('saveService', function(playerService, $rootScope) {
+	this.saveGame = function() {
+		// Save everything in the game service that isn't a function as JSON to local storage
+		for(var prop in playerService) {
+			if(typeof(playerService[prop]) != "function")
+				localStorage.setItem("CompanyGame." + prop, JSON.stringify(playerService[prop]));
+		}
+
+		localStorage.setItem("CompanyGame.lastSaveDate", new Date());
+		$rootScope.$broadcast('displayMessage', 'Game Saved!');
+	}
+});
+
+idleGame.service('importExportService', function($rootScope, cacheService, playerService, gameService) {
+	this.loadGame = function(file) {
+		// Grab current items.  Will be useful if more items were added after you started game
+		var curr = playerService.items;
+
+		// Go through each property of playerService, and if it exists, load it
+		for(var prop in playerService) {
+			var propName = "CompanyGame." + prop;
+			if(file[propName] != undefined)
+				playerService[prop] = JSON.parse(file[propName]);
+		}
+
+		// Load in any business knowledge items bought for this game
+		$rootScope.$broadcast('loadKnowledge');
+
+		// Update the chached values for click power and money/second
+		$rootScope.$broadcast('updateCache');
+
+		// Add in any money you should have received while the game was not playing
+		var lastSave = file["CompanyGame.lastSaveDate"];
+		if(lastSave) {
+			var date = Date.parse(lastSave);
+			var timePassed = (new Date() - date) / 1000;  // Divide by 1000 to get in seconds
+			var missedMoney = timePassed * cacheService.getMps();
+
+			// Get upgrades that deal with giving money when game isn't on
+			var upgrades = gameService.upgrades.
+				filter(function(d) {
+					return d.isOnLoad;
+				}).
+				filter(function(d) {
+					return playerService.getUpgrade(d.id).id >= 0;
+				});
+
+			for(var i = 0; i < upgrades.length; i++) {
+				var missed = upgrades[i].per * missedMoney;
+				playerService.money += missed;
+				playerService.totalMoney += missed;
+			}
+		}
+	}
+});
+
 idleGame.service('cacheService', function($rootScope, gameService, playerService) {
 	this.cachedMps = 0;
 	this.cachedClickPower = 0;
@@ -231,3 +287,18 @@ idleGame.service('cacheService', function($rootScope, gameService, playerService
 		return { mps: mps, resources: resources, research: research };
 	}
 });
+
+// A simple search that will go through the passed in list and find
+// the first item that the specified property matches a given value.
+// Otherwise it will return a default value
+var search = function(list, property, value, def) {
+	for(var item in list)
+		if(list[item][property] === value)
+			return list[item];
+
+	return def;
+}
+
+var isNumeric = function(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
