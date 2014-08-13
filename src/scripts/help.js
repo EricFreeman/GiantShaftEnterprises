1ca7aaa9fbb050b319abfec8d0dfe0c026cb2cb7
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-13 09:11:21 -0600

    Updated the quick start topic for clippy.

--- src/scripts/help.js
+++ src/scripts/help.js
@@ -7,8 +7,9 @@ function HelpController($scope) {
 		{ name: 'Quick Start', questions: [
 			{ name: 'What is the point?', answer: 'Like any business, earn boatloads money!' },
 			{ name: 'What is a Business Opportunity?', answer: 'The big button near the top of the page.' },
-			{ name: 'What should I do first?', answer: 'Complete business opportunities for money, buy items in the first column on the business tab to earn money over time, and then upgrade the items with upgrades from the second column.' },
-			{ name: 'What should I do second?', answer: 'Once you have a good amount of Business Knowledge saved up, you should think about prestiging.' },
+			{ name: 'What should I do first?', answer: 'Follow along with the tutorial and make sure to read all the text.  I swear, there is not that much and it really will help answer many of your initial questions.' },
+			{ name: 'What should I do second?', answer: 'Complete business opportunities for money, buy items in the first column on the business tab to earn money over time, and then upgrade the items with upgrades from the second column.' },
+			{ name: 'What should I do third?', answer: 'Once you have a good amount of Business Knowledge saved up, you should think about prestiging.' },
 		]},
 		{ name: 'Prestige', questions: [
 			{ name: 'What is prestiging?', answer: 'Prestiging means starting a new business and using your Business Knowledge to unlock powerful items, upgrades, and even new sections of the game!' },
